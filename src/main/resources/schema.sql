51bbeccaeddc45e9b2b6e614cb8a30833cb80fe2
Макс <maksim5494@yandex.ru>
2026-05-01 23:57:10 +0500

    правки//

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -59,9 +59,9 @@ CREATE TABLE IF NOT EXISTS feed_events (
     entity_id BIGINT NOT NULL
     );
 
-CREATE TABLE IF NOT EXISTS review_reactions (
-    review_id BIGINT NOT NULL REFERENCES reviews(review_id) ON DELETE CASCADE,
-    user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS review_likes (
+    review_id BIGINT REFERENCES reviews(review_id) ON DELETE CASCADE,
+    user_id BIGINT REFERENCES users(user_id) ON DELETE CASCADE,
     is_like BOOLEAN NOT NULL,
     PRIMARY KEY (review_id, user_id)
     );
