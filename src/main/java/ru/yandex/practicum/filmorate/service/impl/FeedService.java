b44373e13cb700886ea0ff52bbd65bfb4cff984d
Макс <maksim5494@yandex.ru>
2026-05-01 18:52:51 +0500

    исправление ошибок/

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/FeedService.java
@@ -1,17 +1,16 @@
-package ru.yandex.practicum.filmorate.repository.impl;
+package ru.yandex.practicum.filmorate.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.FeedEvent;
 import ru.yandex.practicum.filmorate.repository.FeedStorage;
 import ru.yandex.practicum.filmorate.repository.UserStorage;
-import ru.yandex.practicum.filmorate.service.FeedService;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class FeedServiceImpl implements FeedService {
+public class FeedService implements ru.yandex.practicum.filmorate.service.FeedService {
     private final FeedStorage feedStorage;
     private final UserStorage userStorage;
 
