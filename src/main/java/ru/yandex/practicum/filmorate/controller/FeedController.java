c60bdb1aef97f76973306a676a53821a2bccbde7
Макс <maksim5494@yandex.ru>
2026-05-06 00:10:52 +0500

    1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
@@ -11,6 +11,17 @@ import ru.yandex.practicum.filmorate.service.FeedService;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.FeedEvent;
+import ru.yandex.practicum.filmorate.service.FeedService;
+
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
